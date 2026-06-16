1a46640ef931142ba4904febb6563ba6b3f3da1c
AkashGupta <akashcomputerbisalpur@gmail.com>
2026-06-16 16:36:35 +0530

    db

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://kushalyouth.com",
+        target: "https://kushalyouth.com",
         changeOrigin: true,
         secure: false,
       },
